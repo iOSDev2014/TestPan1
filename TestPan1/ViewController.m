df99657f857a2b96f8168c20b170fff658bea7ae
Forrest Shi <forrest.lin@sap.com>
2014-10-22 18:47:38 +0800

    init

--- TestPan1/ViewController.m
+++ TestPan1/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+
+    UIView *redCircle;
+}
 
 @end
 
@@ -17,6 +20,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    redCircle = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    redCircle.backgroundColor = [UIColor redColor];
+    
+    [self.view addSubview:redCircle];
+    
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onPan:)];
+    //[redCircle addGestureRecognizer:panGesture];
+    
+    [self.view addGestureRecognizer:panGesture];
+    
+    self.view.backgroundColor = [UIColor yellowColor];
+    
+}
+
+- (void)onPan:(UIGestureRecognizer*)gesture{
+    
+    
+    CGPoint curPos = [gesture locationInView:self.view];
+
+    NSLog(@"cur pos %@", NSStringFromCGPoint(curPos));
+    redCircle.center = curPos;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
